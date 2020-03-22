035d88c0966ed4a5e141314d10158957ae094828
Akshit Garg <garg.akshit@gmail.com>
2020-03-23 00:34:49 +0530

    Attempt 1 to fix remote playback

--- index.js
+++ index.js
@@ -1,36 +1,67 @@
-let playing = [];
+const data = [
+  "Brook",
+  "Creek",
+  "Stream",
+  "CloseWaterfall",
+  "DistantWaterfall",
+  "CalmShore",
+  "Shore",
+  "WildShore",
+  "OceanWaves",
+  "LargeWaves",
+  "RainDrops",
+  "PouringRain",
+  "DistantThunder",
+  "CloserThunder",
+  "CoastalWind",
+  "ForestWind",
+  "AutumnBreeze",
+  "Birds",
+  "Frogs",
+  "SummerNight",
+  "HeatWave",
+  "Bonfire",
+  "CoffeeHouse",
+  "CocktailVoices",
+  "MeditationTime",
+  "WindChimes",
+  "FanNoise",
+  "BrownNoise",
+  "PinkNoise",
+  "WhiteNoise"
+];
 
-async function play(name) {
-  if (playing.filter(p => p.name === name).length === 1) {
-    const audio = playing.filter(p => p.name === name)[0].audio;
-    await audio.pause();
+const channels = data.map(d => ({
+  sound: new Howl({
+    src: `./sounds/${d}.ogg`,
+    autoplay: false,
+    loop: true,
+    preload: true
+  }),
+  name: d,
+  playing: false
+}));
 
-    playing = playing.filter(p => p.name !== name);
+function init() {}
+
+function play(name) {
+  const channel = channels.filter(channel => channel.name === name)[0];
+
+  if (channel.playing) {
+    channel.sound.pause();
+    channel.playing = false;
 
     document.getElementById(name).style.backgroundColor = "beige";
   } else {
-    const audio = new Audio(`./sounds/${name}.ogg`);
-    audio.loop = true;
+    document.getElementById(name).style.backgroundColor = "orange";
 
-    await audio.play();
+    channel.sound.play();
+    channel.playing = true;
 
-    playing.push({
-      name,
-      audio
-    });
     document.getElementById(name).style.backgroundColor = "#90ee90";
   }
 }
 
 function egg() {
-  (function(c) {
-    var b = c.getElementsByTagName("head")[0];
-    a = c.createElement("script");
-    a.type = "text/javascript";
-    a.src = unescape(
-      "%2F%2Fdl.dropboxusercontent.com%2Fs%2F82526m7wt4vpesr%2Fharlem-shake.js"
-    );
-    a.async = true;
-    b.appendChild(a);
-  })(document);
+  document.getElementById("t").innerHTML = "Sorry I nuked the egg!";
 }
